59c36020607acd0786548fe53ed587028820cd2e
Ваше Имя <deucela.pavel@gmail.com>
2019-10-30 11:32:51 +0300

    2 step

--- src/bin/gendiff.js
+++ src/bin/gendiff.js
@@ -6,6 +6,11 @@ const test = () => {
   commander
     .version('0.0.1')
     .description('Compares two configuration files and shows a difference.')
+    .option('-f, --format [type]', 'Output format')
+    .arguments('')
+    .action(() => {
+      console.log('gendiff -h');
+    })
     .parse(process.argv);
 };
 
