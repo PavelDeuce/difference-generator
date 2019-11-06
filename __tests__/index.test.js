8d81b3972cbf3b12fe567084158cfef71719b62c
Ваше Имя <deucela.pavel@gmail.com>
2019-11-06 16:13:13 +0300

    4 step 3/4

--- __tests__/index.test.js
+++ __tests__/index.test.js
@@ -1,16 +1,11 @@
+import { readFileSync } from 'fs';
+import path from 'path';
 import genDiff from '../src/index';
 
 test('test for json content', () => {
-  const pathToBefore = './__tests__/__fixtures__/configs/before.json';
-  const pathToAfter = './__tests__/__fixtures__/configs/after.json';
-  const result = `{
-   host: hexlet.io
- - timeout: 50
- + timeout: 20
- - proxy: 123.234.53.22
- - follow: false
- + verbose: true
-}`;
+  const pathToBefore = path.join(__dirname, '/__fixtures__/configs/', 'before.json');
+  const pathToAfter = path.join(__dirname, '/__fixtures__/configs/', 'after.json');
+  const result = readFileSync(path.join(__dirname, '/__fixtures__/result/', 'result.txt'), 'utf8');
 
   expect(genDiff(pathToBefore, pathToAfter)).toBe(result);
 });
