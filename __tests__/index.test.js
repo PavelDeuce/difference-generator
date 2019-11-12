1d57ab19fac574d087f018161e093f111a52be85
Ваше Имя <deucela.pavel@gmail.com>
2019-11-12 22:51:28 +0300

    refactot tests

--- __tests__/index.test.js
+++ __tests__/index.test.js
@@ -1,67 +1,29 @@
 import { readFileSync } from 'fs';
 import path from 'path';
-import genDiff from '../src/index';
+import gendiff from '../src/index';
 
-test('test for json content', () => {
-  const pathToBefore = path.join(__dirname, '/__fixtures__/configs/', 'before.json');
-  const pathToAfter = path.join(__dirname, '/__fixtures__/configs/', 'after.json');
-  const result = readFileSync(path.join(__dirname, '/__fixtures__/result/', 'result.txt'), 'utf8');
+describe('Compares two configuration files', () => {
+  test.each`
+   extname       |   format
+    ${'json'}    |    ${'nested'}
+    ${'json'}    |    ${'plain'}
+    ${'json'}    |    ${'json'}
+    ${'ini'}     |    ${'json'}
+    ${'ini'}     |    ${'nested'}
+    ${'yaml'}    |    ${'plain'}
+    ${'yaml'}    |    ${'nested'}
+`('gendiff before.$extname after.$extname --format $format',
 
-  expect(genDiff(pathToBefore, pathToAfter)).toBe(result);
-});
 
-test('test for yaml content', () => {
-  const pathToBefore = path.join(__dirname, '/__fixtures__/configs/', 'before.yaml');
-  const pathToAfter = path.join(__dirname, '/__fixtures__/configs/', 'after.yaml');
-  const result = readFileSync(path.join(__dirname, '/__fixtures__/result/', 'result.txt'), 'utf8');
+  ({ extname, format }) => {
+    const commonPath = path.join(__dirname, '/__fixtures__/');
 
-  expect(genDiff(pathToBefore, pathToAfter)).toBe(result);
-});
+    const pathToBefore = path.join(commonPath, 'configs', extname, `before.${extname}`);
+    const pathToAfter = path.join(commonPath, 'configs', extname, `after.${extname}`);
+    const pathToResult = path.join(commonPath, 'result', `${format}Result.txt`);
 
-test('test for ini content', () => {
-  const pathToBefore = path.join(__dirname, '/__fixtures__/configs/', 'before.ini');
-  const pathToAfter = path.join(__dirname, '/__fixtures__/configs/', 'after.ini');
-  const result = readFileSync(path.join(__dirname, '/__fixtures__/result/', 'result.txt'), 'utf8');
+    const result = readFileSync(pathToResult, 'utf-8');
 
-  expect(genDiff(pathToBefore, pathToAfter)).toBe(result);
-});
-
-test('test for plain content json', () => {
-  const pathToBefore = path.join(__dirname, '/__fixtures__/configs/', 'before.json');
-  const pathToAfter = path.join(__dirname, '/__fixtures__/configs/', 'after.json');
-  const result = readFileSync(path.join(__dirname, '/__fixtures__/result/', 'resultPlain.txt'), 'utf8');
-
-  expect(genDiff(pathToBefore, pathToAfter, 'plain')).toBe(result);
-});
-
-test('test for plain content yaml', () => {
-  const pathToBefore = path.join(__dirname, '/__fixtures__/configs/', 'before.yaml');
-  const pathToAfter = path.join(__dirname, '/__fixtures__/configs/', 'after.yaml');
-  const result = readFileSync(path.join(__dirname, '/__fixtures__/result/', 'resultPlain.txt'), 'utf8');
-
-  expect(genDiff(pathToBefore, pathToAfter, 'plain')).toBe(result);
-});
-
-test('test for plain content ini', () => {
-  const pathToBefore = path.join(__dirname, '/__fixtures__/configs/', 'before.ini');
-  const pathToAfter = path.join(__dirname, '/__fixtures__/configs/', 'after.ini');
-  const result = readFileSync(path.join(__dirname, '/__fixtures__/result/', 'resultPlain.txt'), 'utf8');
-
-  expect(genDiff(pathToBefore, pathToAfter, 'plain')).toBe(result);
-});
-
-test('test for json content json', () => {
-  const pathToBefore = path.join(__dirname, '/__fixtures__/configs/', 'before.json');
-  const pathToAfter = path.join(__dirname, '/__fixtures__/configs/', 'after.json');
-  const result = readFileSync(path.join(__dirname, '/__fixtures__/result/', 'resultJson.txt'), 'utf8');
-
-  expect(genDiff(pathToBefore, pathToAfter, 'json')).toBe(result);
-});
-
-test('test for json content ini', () => {
-  const pathToBefore = path.join(__dirname, '/__fixtures__/configs/', 'before.ini');
-  const pathToAfter = path.join(__dirname, '/__fixtures__/configs/', 'after.ini');
-  const result = readFileSync(path.join(__dirname, '/__fixtures__/result/', 'resultJson.txt'), 'utf8');
-
-  expect(genDiff(pathToBefore, pathToAfter, 'json')).toBe(result);
+    expect(gendiff(pathToBefore, pathToAfter, format)).toBe(result);
+  });
 });
