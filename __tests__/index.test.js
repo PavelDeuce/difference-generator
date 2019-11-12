f4303d88e15a83b0335efc98a74649024a64c2c2
Ваше Имя <deucela.pavel@gmail.com>
2019-11-12 21:51:16 +0300

    step 9

--- __tests__/index.test.js
+++ __tests__/index.test.js
@@ -49,3 +49,27 @@ test('test for plain content ini', () => {
 
   expect(genDiff(pathToBefore, pathToAfter, 'plain')).toBe(result);
 });
+
+test('test for json content json', () => {
+  const pathToBefore = path.join(__dirname, '/__fixtures__/configs/', 'before.json');
+  const pathToAfter = path.join(__dirname, '/__fixtures__/configs/', 'after.json');
+  const result = readFileSync(path.join(__dirname, '/__fixtures__/result/', 'resultJson.txt'), 'utf8');
+
+  expect(genDiff(pathToBefore, pathToAfter, 'json')).toBe(result);
+});
+
+test('test for json content yaml', () => {
+  const pathToBefore = path.join(__dirname, '/__fixtures__/configs/', 'before.yaml');
+  const pathToAfter = path.join(__dirname, '/__fixtures__/configs/', 'after.yaml');
+  const result = readFileSync(path.join(__dirname, '/__fixtures__/result/', 'resultJson.txt'), 'utf8');
+
+  expect(genDiff(pathToBefore, pathToAfter, 'json')).toBe(result);
+});
+
+test('test for json content ini', () => {
+  const pathToBefore = path.join(__dirname, '/__fixtures__/configs/', 'before.ini');
+  const pathToAfter = path.join(__dirname, '/__fixtures__/configs/', 'after.ini');
+  const result = readFileSync(path.join(__dirname, '/__fixtures__/result/', 'resultJson.txt'), 'utf8');
+
+  expect(genDiff(pathToBefore, pathToAfter, 'json')).toBe(result);
+});
--- src/formatters/index.js
+++ src/formatters/index.js
@@ -1,10 +1,13 @@
 import plainRender from './plain';
 import nestedRender from './nested';
+import jsonRender from './json';
 
 export default (elements, format) => {
   switch (format) {
     case 'plain':
       return plainRender(elements);
+    case 'json':
+      return jsonRender(elements);
     default:
       return nestedRender(elements);
   }
