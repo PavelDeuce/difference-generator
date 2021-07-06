b2e7fa28db2c4b14d12f9dd1e3a64e1ba45e262e
PavelDeuce <deucela.pavel@gmail.com>
2021-07-07 00:16:30 +0300

    additional test to index.test.js

--- __tests__/index.test.js
+++ __tests__/index.test.js
@@ -4,24 +4,24 @@ import path from 'path';
 import gendiff from '../src';
 import { formatMapping } from '../src/utils';
 
+const commonPath = path.join(__dirname, '/__fixtures__/');
+const configsDirName = 'configs';
+
 describe('Compares two configuration files', () => {
   test.each`
     extname   | format
     ${'json'} | ${formatMapping.nested}
     ${'json'} | ${formatMapping.plain}
     ${'json'} | ${formatMapping.json}
-    ${'ini'}  | ${formatMapping.json}
-    ${'ini'}  | ${formatMapping.nested}
     ${'yaml'} | ${formatMapping.plain}
     ${'yaml'} | ${formatMapping.nested}
+    ${'ini'}  | ${formatMapping.nested}
     ${'ini'}  | ${formatMapping.json}
     ${'ini'}  | ${formatMapping.plain}
   `(
     'gendiff before.$extname after.$extname --format $format',
 
     ({ extname, format }) => {
-      const commonPath = path.join(__dirname, '/__fixtures__/');
-      const configsDirName = 'configs';
       const pathToBefore = path.join(commonPath, configsDirName, extname, `before.${extname}`);
       const pathToAfter = path.join(commonPath, configsDirName, extname, `after.${extname}`);
       const pathToResult = path.join(commonPath, 'result', `${format}Result.txt`);
@@ -29,4 +29,12 @@ describe('Compares two configuration files', () => {
       expect(gendiff(pathToBefore, pathToAfter, format)).toBe(result);
     }
   );
+
+  test('gendiff should return "nested" without param', () => {
+    const pathToBefore = path.join(commonPath, configsDirName, 'json', 'before.json');
+    const pathToAfter = path.join(commonPath, configsDirName, 'json', 'after.json');
+    const pathToResult = path.join(commonPath, 'result', 'nestedResult.txt');
+    const result = readFileSync(pathToResult, 'utf-8');
+    expect(gendiff(pathToBefore, pathToAfter)).toBe(result);
+  });
 });
